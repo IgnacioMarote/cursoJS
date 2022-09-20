c54b38b9f71145d8452d08e324c519b0811b16c3
Ignacio Marote <ignaciomarote@hotmail.com>
2022-09-20 20:06:04 -0300

    clase 6 desafio

---
+++ Clase 6/desafio.js
@@ -0,0 +1,34 @@
+//VARIABLES GLOBALES
+let ingresoTotal = 0; 
+let gastoTotal = 0;
+const ingresoLista = [];
+const gastoLista = [];
+//FUNCIONES
+const suma = (total, item) => total + item;
+const resta = (totIngreso, totGasto) => totIngreso - totGasto;
+
+//DESARROLLO
+let nombre = prompt("Ingresa tu nombre");
+let ingreso = parseInt(prompt(`${nombre} agrega un ingreso`));
+while(ingreso != 0){
+    ingresoLista.push(ingreso);
+    /* ingresoTotal += ingreso; */
+    ingreso = parseInt(prompt("Agrega un ingreso"));
+}
+let gasto = parseInt(prompt(`${nombre} agrega un gasto`));
+while(gasto!=0){
+    gastoLista.push(gasto);
+    /* gastoTotal += gasto; */
+    gasto = parseInt(prompt("Agrega un gasto"));
+}
+for (let i = 0; i < ingresoLista.length; i++) {
+    const ingresoItem = ingresoLista[i];
+    ingresoTotal = suma(ingresoTotal, ingresoItem);
+}
+for (let x = 0; x < gastoLista.length; x++) {
+    const gastoItem = gastoLista[x];
+    gastoTotal = suma(gastoTotal, gastoItem);
+}
+
+let balance = resta(ingresoTotal, gastoTotal);
+alert(`El balance es de: $${balance}`)
