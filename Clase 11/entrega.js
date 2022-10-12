e944ef2ba354d3339297a2469da327dbacaadcc5
Ignacio Marote <ignaciomarote@hotmail.com>
2022-10-11 23:31:05 -0300

    Desafio complementario

--- Clase 11/entrega.js
+++ Clase 11/entrega.js
@@ -1,9 +1,7 @@
 //VARIABLES GLOBALES
-let ingresoTotal = 0;
 const ingresoLista = [];
 const gastoLista = [];
 //FUNCIONES
-const suma = (total, item) => total + item;
 const resta = (totIngreso, totGasto) => totIngreso - totGasto;
 
 //DESARROLLO
@@ -14,45 +12,49 @@ let listIng = document.getElementById("listaIngresos");
 let formIng = document.getElementById("formIngresos");
 formIng.addEventListener(`submit`, function (e) {
         e.preventDefault();
+        valueIng.value === "" && (valueIng.value = 0); //Agregando OPERADOR LÓGICO AND para optimizar el código
         ingresoLista.push(valueIng.value);
 
         const nameIngreso = nameIng.value;
         const valorIngreso = valueIng.value;
 
-        const itemIngreso = { nombre: nameIngreso, valor: valorIngreso}
-        const itemJSON = JSON.stringify(itemIngreso);
-        console.log(itemJSON);
+        const itemIngreso = {nombre: nameIngreso, valor: valorIngreso}
+        const itemIngJSON = JSON.stringify(itemIngreso);
 
-        sessionStorage.setItem(nameIngreso, itemJSON);
-        const objJSON = sessionStorage.getItem(nameIngreso);
-        const objetJSON = JSON.parse(objJSON);
-        console.log(objJSON);
-        console.log(objetJSON);
-        console.log(objetJSON.nombre);
-        console.log(objetJSON.valor);
-        listIng.innerHTML += `<li>Nombre: ${objetJSON.nombre} Valor: ${objetJSON.valor} </li>`;
+        sessionStorage.setItem(nameIngreso, itemIngJSON);
+        const objIngJSON = sessionStorage.getItem(nameIngreso);
+        const objetIngJSON = JSON.parse(objIngJSON);
+        listIng.innerHTML += `<li>Nombre: ${objetIngJSON.nombre} Valor: ${objetIngJSON.valor} </li>`;
         formIng.reset();
     })
 
-
-let itemGas = document.getElementById("inputGastos");
+let nameGas = document.getElementById("inputGastosNombre");
+let valueGas = document.getElementById("inputGastosValor");
 let listGas = document.getElementById("listaGastos");
 let formGas = document.getElementById("formGastos");
 formGas.addEventListener(`submit`, function (e) {
-        e.preventDefault();
-        gastoLista.push(itemGas.value);
-        listGas.innerHTML += `<li>${itemGas.value}</li>`;
-        formGas.reset();
+    e.preventDefault();
+    valueGas.value === "" && (valueGas.value = 0);
+    gastoLista.push(valueGas.value);
+
+    const nameGastos = nameGas.value;
+    const valorGastos = valueGas.value;
+
+    const itemGastos = {nombre: nameGastos, valor: valorGastos}
+    const itemGasJSON = JSON.stringify(itemGastos);
+
+    sessionStorage.setItem(nameGastos, itemGasJSON);
+    const objGasJSON = sessionStorage.getItem(nameGastos);
+    const objetGasJSON = JSON.parse(objGasJSON);
+    listGas.innerHTML += `<li>Nombre: ${objetGasJSON.nombre} Valor: ${objetGasJSON.valor} </li>`;
+    formGas.reset();
     })
 
-for (let i = 0; i < ingresoLista.length; i++) {
-    const ingresoItem = ingresoLista[i];
-    ingresoTotal = suma(ingresoTotal, ingresoItem);
-}
-const gastoTotal = gastoLista.reduce((acumulador, item) => acumulador + item, 0);
-
-let balance = resta(ingresoTotal, gastoTotal);
-/* alert(`El balance es de: $${balance}`) */
-
-let itemBalance = document.getElementById("lista__balance")
-itemBalance.innerHTML = `El balance es de ${balance}`;
+let itemBalance = document.getElementById("listaBalance")
+formGas.addEventListener(`submit`, function () {
+    itemBalance.innerHTML = ` `;
+    const ingresoTotal = ingresoLista.reduce((acumulador, item) => acumulador + parseInt(item), 0);
+    const gastoTotal = gastoLista.reduce((acumulador, item) => acumulador + parseInt(item), 0);
+    const balance = resta(ingresoTotal, gastoTotal);
+    itemBalance.innerHTML += `El balance es de ${balance}`;
+})
